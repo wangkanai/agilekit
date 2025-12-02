53e09422ac9e7fe3524930ec2e6e804e997215a6
wangkanai <sarin@wangkanai.com>
2025-12-02 08:42:59 +0700

    feat: add chalk dependency and update CLI welcome message styling

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,14 @@ import './commands/upgrade.js';
 import './commands/check.js';
 
 import { Command } from 'commander';
+import chalk from 'chalk';
+
 const program = new Command();
 
-program.name('agilekit').version('1.0.0', '-v, --version', 'output the version number').description('AgileKit CLI tool');
+program.name('agilekit')
+    .version('1.0.0', '-v, --version', 'output the version number')
+    .description('AgileKit CLI tool');
 
 program.parse(process.argv);
 
-console.log('Welcome to AgileKit CLI! Use --help to see available commands.');
+console.log(chalk.cyan('Welcome to AgileKit CLI!'), chalk.gray('Use --help to see available commands.'));
